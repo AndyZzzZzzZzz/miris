a13afe4bc1fc5811135c2e6103b2b6c4b6f7b427
AndyZzzZzzZzz <zkpandy@outlook.com>
2025-06-22 22:29:29 -0700

    Add notes to plan

--- plan.go
+++ plan.go
@@ -1,5 +1,12 @@
 package main
 
+/*
+go run plan.go shibuya 16 0.9
+1. query predicates: loads shibuya
+2. sampling frequency: 16
+3. precision/recall bound: 0.9
+
+*/
 import (
 	"github.com/mitroadmaps/miris/data"
 	"github.com/mitroadmaps/miris/miris"
@@ -12,22 +19,38 @@ import (
 )
 
 func main() {
+	// predicate name
 	predName := os.Args[1]
+	// sampling frequency (1 frame out of every N)
 	freq, _ := strconv.Atoi(os.Args[2])
+	// target a value (desired precision/recall lower bound)
 	bound, _ := strconv.ParseFloat(os.Args[3], 64)
-
+	
+	// optional: if a prev plan file is passed
+	// this loads it and reuses the sample quality scores
 	var existingPlan miris.PlannerConfig
 	var qSamples map[int][]float64
 	if len(os.Args) >= 5 {
 		miris.ReadJSON(os.Args[4], &existingPlan)
 		qSamples = existingPlan.QSamples
 	}
-
+	
+	// loads meta data
+	// ppCgf: preprocessing config (bounding boxes, track setup)
+	// modelCfg: Model config
 	ppCfg, modelCfg := data.Get(predName)
-
+	
+	/*
+	If we didn't load an existing plan, generate new
+	Quality curves: estimated tradeoffs between query accuracy and model effort, precomputed across different q values
+	(thresholds for filtering/uncertainty)
+	*/
 	if qSamples == nil {
 		qSamples = planner.GetQSamples(2*freq, ppCfg, modelCfg)
 	}
+
+	// Uses the quality samples to choose the best q value
+	// Goal: maximize speed while keeping disired accuracy
 	q := planner.PlanQ(qSamples, bound)
 	log.Println("finished planning q", q)
 	plan := miris.PlannerConfig{
