c83c4399a681aefed99f757b07eb96268fd82fa0
AndyZzzZzzZzz <zkpandy@outlook.com>
2025-06-22 22:12:23 -0700

    Notes for query configuration

--- predicate/tmc.go
+++ predicate/tmc.go
@@ -1,22 +1,33 @@
 package predicate
 
 // turning movement count (TMC) queries
-
+/*
+	Represent pre-registered query configurations that MIRIS can reference by name during planning and execution
+	Not full queries but define the core logic of what the query is looking for, directly used when a named query is invoked.
+*/
 import (
 	"github.com/mitroadmaps/gomapinfer/common"
 )
 
+/*
+init() function is automatically called when the package is imported. Here, it is used to register query predicates into the
+global predicates map.
+*/
 func init() {
+	// object starts in lower-left region and ends in an upper-right section
 	predicates["uav"] = StartEndPredicate(
 		common.Rect(362, 446, 706, 1080).ToPolygon(),
 		common.Rect(784, 176, 1920, 642).ToPolygon(),
 	)
-
+	
+	// left to right
+	// object must pass through left region and right region
 	predicates["warsawlr"] = WaypointPredicate([]common.Polygon{
 		common.Rect(0, 610, 930, 1080).ToPolygon(),
 		common.Rect(1190, 700, 1920, 1080).ToPolygon(),
 	})
 
+	// object must pass through both in order
 	predicates["warsawtb"] = WaypointPredicate([]common.Polygon{
 		{
 			common.Point{978, 337},
@@ -34,6 +45,8 @@ func init() {
 		},
 	})
 
+	// highway movement
+	// describe distinct movement pattern
 	predicates["warsawhw"] = WaypointPredicate([]common.Polygon{
 		{
 			common.Point{1314, 403},
@@ -48,8 +61,10 @@ func init() {
 			common.Point{1920, 550},
 		},
 	})
-
+	
+	// matches if any of the three movement patterns are observed
 	predicates["warsaw"] = Or(predicates["warsawlr"], predicates["warsawtb"], predicates["warsawhw"])
+	
 
 	shibuyaPolys := map[string]common.Polygon{
 		"right": {
